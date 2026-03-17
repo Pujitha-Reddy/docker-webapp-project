d9af65d04f8dd70842a8dddeb47b455aa7f26777
Pujitha <pujithamalladi25@gmail.com>
2026-03-17 18:32:28 -0500

    Add CI/CD pipeline with GitHub Actions

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello from my Dockerized App!</h1><p>Running on EC2</p>');
+  res.send('<h1>Hello from my Dockerized App!</h1><p>Version 2 — Auto-deployed with GitHub Actions CI/CD!</p>');
 });
 
 app.listen(PORT, () => {
